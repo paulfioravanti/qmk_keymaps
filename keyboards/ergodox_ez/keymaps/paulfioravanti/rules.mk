2eabdb2dd7fa4a16c46b39c0d6e8851900023785
Paul Fioravanti <paul.fioravanti@gmail.com>
2018-10-12 19:10:31 +1100

    Add more comments around why mouse keys have to be disabled when using steno

--- keyboards/ergodox_ez/keymaps/paulfioravanti/rules.mk
+++ keyboards/ergodox_ez/keymaps/paulfioravanti/rules.mk
@@ -1,6 +1,11 @@
 STENO_ENABLE = yes
 # Needed for Steno
 VIRTSER_ENABLE = yes
-# FORCE_NKRO = yes
-# Mouse keys can't be used if Steno is enabled.
+FORCE_NKRO = yes
+# Mouse keys can't be used if Steno is enabled, so Steno gets priority for now.
+# Attempting to compile with steno and mouse keys enabled results in the
+# following error:
+# `"There are not enough available endpoints to support all functions.
+# Remove some in the rules.mk file.
+# (MOUSEKEY, EXTRAKEY, CONSOLE, NKRO, MIDI, SERIAL, STENO)"`
 MOUSEKEY_ENABLE = no
