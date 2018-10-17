db08a78c55f46b1c64c0596978693c3fcc80f779
Paul Fioravanti <paul.fioravanti@gmail.com>
2018-10-17 15:04:47 +1100

    Add more comments around the NKRO config

--- keyboards/ergodox_ez/keymaps/paulfioravanti/config.h
+++ keyboards/ergodox_ez/keymaps/paulfioravanti/config.h
@@ -26,5 +26,8 @@
 #define TAPPING_TERM 50
 
 // Keep N-key rollover (NKRO) on by default
+// What is NKRO:
 // https://en.wikipedia.org/wiki/Rollover_(key)#n-key_rollover
+// Reference for why this line needs to be here:
+// https://github.com/qmk/qmk_firmware/issues/1695#issuecomment-328346224
 #define FORCE_NKRO
