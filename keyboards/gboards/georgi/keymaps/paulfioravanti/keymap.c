e993aab38523ae7722ed34215d2d5d1a357d8f1f
Paul Fioravanti <paul.fioravanti@gmail.com>
2022-07-06 14:15:17 +1000

    Update firmware to erase any text entered before steno mode is changed over to

--- keyboards/gboards/georgi/keymaps/paulfioravanti/keymap.c
+++ keyboards/gboards/georgi/keymaps/paulfioravanti/keymap.c
@@ -26,9 +26,10 @@
 #define GAMING_2      2
 
 // HID Commands
-#define TOGGLE_GAMING 1
-#define SWITCH_TO_GAMING 2
-#define SWITCH_TO_STENO  3
+#define TOGGLE_DOOM_TYPIST_MODE 1
+#define TOGGLE_GAMING           2
+#define SWITCH_TO_GAMING        3
+#define SWITCH_TO_STENO         4
 
 /* Keyboard Layout
  * ,---------------------------------.    ,------------------------------.
@@ -559,6 +560,19 @@ void raw_hid_receive(uint8_t *data, uint8_t length) {
     // data is the packet received from host.
     // We only care about the integer values in the first array element.
     switch (*data) {
+      case TOGGLE_DOOM_TYPIST_MODE:
+        if (biton32(layer_state) == GAMING) {
+          // Doom Typist (Ctrl-Backspace):
+          // Erase any text that may have been entered inadvertently just
+          // before steno mode is selected.
+          SEND_STRING(SS_DOWN(X_LCTRL));
+          SEND_STRING(SS_TAP(X_BACKSPACE));
+          SEND_STRING(SS_UP(X_LCTRL));
+          layer_off(GAMING);
+        } else {
+          layer_on(GAMING);
+        }
+        break;
       case TOGGLE_GAMING:
         if (biton32(layer_state) == GAMING) {
           layer_off(GAMING);
